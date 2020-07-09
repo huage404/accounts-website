41cd04cea9658ca67fedf1eda54ea1af9ef705a1
去年夏天 <6523611+zhenhua4207@user.noreply.gitee.com>
2020-07-10 00:00:07 +0800

    deploy

---
+++ precache-manifest.8c34b043dc8964b7a85825156a25aa02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9a49e408030cd8bc48",
+    "url": "/accounts-website/css/app.a5d4cd28.css"
+  },
+  {
+    "revision": "6b99b2833949406e3c9c2d76da73e0c4",
+    "url": "/accounts-website/index.html"
+  },
+  {
+    "revision": "7f9a49e408030cd8bc48",
+    "url": "/accounts-website/js/app.e21c3775.js"
+  },
+  {
+    "revision": "b244161a472aca78e4a7",
+    "url": "/accounts-website/js/chunk-vendors.4842461f.js"
+  },
+  {
+    "revision": "24d38333617760256148f796c9bfe91d",
+    "url": "/accounts-website/manifest.json"
+  },
+  {
+    "revision": "8c22ec3fb32cc4cc647d63fd65c34db5",
+    "url": "/accounts-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/accounts-website/robots.txt"
+  }
+]);
